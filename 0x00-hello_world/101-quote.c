754e48fafe4557be717d52efd033301eaddf31d8
glackyy <othmanesticky@gmail.com>
2023-03-16 04:07:23 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,5 +5,5 @@
  * Return: 0 (Success)
  */
 int main(void)
-{fput("and that piece of art is useful\" -Dora Korpar, 2015-10-19", stdout);
+{fputs("and that piece of art is useful\" -Dora Korpar, 2015-10-19", stdout);
 return (1); }

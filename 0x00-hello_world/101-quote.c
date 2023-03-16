ad180e135482d083a8cd715daf6243c8c8adfe83
glackyy <glackycode@example.com>
2023-03-16 04:10:06 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,5 +5,5 @@
  * Return: 0 (Success)
  */
 int main(void)
-{fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
+{fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1); }

fa3147a7fa86dda2290c8f881d8f9afecc1e30d3
glackyy <glackycode@example.com>
2023-03-16 03:34:09 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+  * main - A program that print a line with printf fonction
+  * Return: 0 (Success)
+  */
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0); }
+

2a2ade596c239a6165d37e84d8df884722804b29
glackyy <othmanesticky@gmail.com>
2023-03-16 04:05:40 -0700

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{fput("and that piece of art is useful\" -Dora Korpar, 2015-10-19", stdout);
+return (1); }

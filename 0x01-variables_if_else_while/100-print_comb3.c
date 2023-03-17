f51ea90b174c20073e3c9119357d82c646272915
glackyy <glackycode@example.com>
2023-03-17 06:43:33 -0700

    print combinations 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int i, j;
+for (i = 0 ; i <= 8 ; i++)
+{
+for (j = i + 1; j <= 9 ; j++)
+{
+putchar('0' + i);
+putchar('0' + j);
+if (i != 8 || j != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
+

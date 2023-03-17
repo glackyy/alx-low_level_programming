a68f48ba361cae70b99410ae85723d0cd08e5b85
glackyy <othmanesticky@gmail.com>
2023-03-17 07:07:13 -0700

    print combination 4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,7 +15,7 @@ for (k = j + 1 ; k <= 9 ; k++)
 putchar('0' + i);
 putchar('0' + j);
 putchar('0' + k);
-if (i != 8 || j != 9 || k != 10)
+if (i != 8 || j != 9 || k != 9)
 {
 putchar(',');
 putchar(' ');

11873a6521bcc63fb7f8d8827c5398b35f926f2b
glackyy <othmanesticky@gmail.com>
2023-03-17 05:41:16 -0700

    print alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int numb;
+for (numb = 'z' ; numb >= 'a' ; numb--)
+putchar(numb);
+putchar('\n');
+return (0);
+}
+

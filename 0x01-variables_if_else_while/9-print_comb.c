12e2fac6d6e5de5d17ded3f085100f93010ba070
glackyy <glackycode@example.com>
2023-03-17 06:05:06 -0700

    print combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int digit;
+for (digit = 0 ; digit < 10 ; digit++)
+{
+putchar('0' + digit);
+if (digit != 9)
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
+

156155709a4bbe37386c3e3e0545e1c70bcf192d
glackyy <glackycode@example.com>
2023-03-21 04:41:54 -0700

    print alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,10 +6,12 @@
 void print_alphabet_x10(void)
 {
 int i = 0;
-char alph = 'a';
+char alph;
 while (i < 10)
 {
-while (alph <= 'z'){
+alph = 'a';
+while (alph <= 'z')
+{
 _putchar(alph);
 alph++;
 }

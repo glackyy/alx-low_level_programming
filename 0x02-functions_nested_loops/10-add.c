7459e321fb1bf77f48974b882192c2383f3fc9f1
glackyy <glackycode@example.com>
2023-03-21 06:31:33 -0700

    addition a b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - Entry Point
+ * @a: int
+ * @b: int
+ * Return: Result
+ */
+int add(int a, int b)
+{
+int res;
+res = a + b;
+return (res);
+}
+

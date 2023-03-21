5b024b3185abe8cd6325670bf35a024bf54e158c
glackyy <othmanesticky@gmail.com>
2023-03-21 03:29:37 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry Point
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+char alph;
+for (alph = 'a' ; alph <= 'z' ; alph++)
+_putchar(alph);
+_putchar('\n');
+}
+

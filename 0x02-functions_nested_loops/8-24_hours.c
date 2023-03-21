cb2d74eac69323ab36f8e22af7a77b6aab4eb62d
glackyy <othmanesticky@gmail.com>
2023-03-21 06:04:02 -0700

    24 hours chrono

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - Entry Point
+ */
+void jack_bauer(void)
+{
+int i,j;
+i = 0 ;
+while (i < 24)
+{
+j = 0;
+while (j < 60)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0');
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+_putchar('\n');
+j++;
+}
+i++;
+}
+}
+

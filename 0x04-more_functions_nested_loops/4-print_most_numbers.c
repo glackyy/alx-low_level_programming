8ab66b6d3cda0cc6a9be9b8f7fbffbeacacea299
glackyy <othmanesticky@gmail.com>
2023-03-23 03:00:15 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_most_numbers - Entry Function
+ * Return: 0 (Success)
+ */
+void print_most_numbers(void)
+{
+int i = 0;
+while (i <= 9)
+{
+	if (i != 2 || i != 4)
+	{
+		_putchar(i + '0');
+	}
+	i++;
+}
+_putchar('\n');
+}

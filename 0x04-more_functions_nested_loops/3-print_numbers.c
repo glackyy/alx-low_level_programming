5b372d9d58c320293eade01c5cc3a7c44e0bd72f
glackyy <glackycode@example.com>
2023-03-23 02:50:12 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_numbers - Entry Function
+ * Return: 0 (Success)
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0 ; i < 10 ; i++)
+_putchar(i + '0');
+_putchar('\n');
+}

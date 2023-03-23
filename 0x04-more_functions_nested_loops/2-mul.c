7671ecab744a3667856dbc2bc743e3b87a828e79
glackyy <glackycode@example.com>
2023-03-23 02:34:28 -0700

    multiplie two int

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - Entry Function
+ * @a: integer
+ * @b: integer
+ * Return: mult (Success)
+ */
+int mul(int a, int b)
+{
+int mult;
+mult = a * b;
+return (mult);
+}

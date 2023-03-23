601f0f5f13a96b89f5c8ca09011a17890ef4e1dd
glackyy <glackycode@example.com>
2023-03-23 05:36:30 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,21 +4,25 @@
  * @size: integer
  * Return: 0 (Success)
  */
-void print_triangle(int size) 
+void print_triangle(int size)
 {
-	int i, j;
-    if (size <= 0) {
-        _putchar('\n');
-        return;
-    }
-
-    for (i = 1 ; i <= size ; i++) {
-        for (j = i ; j < size ; j++) {
-            _putchar(' ');
-        }
-	for (j = 1 ; j <= i ; j++){
-		_putchar('#');}
-        _putchar('\n');
-    }
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+return;
+}
+for (i = 1 ; i <= size ; i++)
+{
+for (j = i ; j < size ; j++)
+{
+_putchar(' ');
+}
+for (j = 1 ; j <= i ; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
 }
 

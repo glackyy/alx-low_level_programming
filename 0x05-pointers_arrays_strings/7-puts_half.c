a866f852b5d254aaa7ebc9e2df9f597e7959a520
glackyy <glackycode@example.com>
2023-03-27 06:52:00 -0700

    puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,13 +7,12 @@ void puts_half(char *str)
 {
 int len = 0;
 int i, n;
-for (len = 0 ; str[len] != '\0' ; len++)
-;
-if (len % 2 == 0)
-n = len / 2;
-else if (len % 2 == 1)
+for (i = 0 ; str[i] != '\0' ; i++)
+len++;
+n = len  / 2;
+if (len % 2 == 1)
 n = (len - 1) / 2;
-for (i = n ; i < len ; i++)
+for (i = n ; str[i] != '\0' ; i++)
 _putchar(str[i]);
 _putchar('\n');
 }

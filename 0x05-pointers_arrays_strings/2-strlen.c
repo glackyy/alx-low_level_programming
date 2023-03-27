a122990f4cc850ee087152e69aa2b332a9fe456f
glackyy <othmanesticky@gmail.com>
2023-03-27 03:56:58 -0700

    length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen - Entry Function
+ * @s: char
+ * Return: len (Success)
+ */
+int _strlen(char *s)
+{
+int len = 0;
+while (*s != '\0')
+{
+	len++;
+	s++;
+}
+return len;
+}

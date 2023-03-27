2c35d65dfeb0ee94dae0c2ad10a70d3e85b19228
glackyy <othmanesticky@gmail.com>
2023-03-27 03:23:41 -0700

    reset_to_98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * reset_to_98 : Entry Function
- * *n@: integer
+ * reset_to_98 -  Entry Function
+ * Changes the actual value to 98
+ * @n: Integer
  */
 void reset_to_98(int *n)
 {

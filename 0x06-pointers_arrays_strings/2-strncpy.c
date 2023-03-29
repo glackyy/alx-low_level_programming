e58edb5f61f3586cdbfa73a071dedf0a5c76e470
glackyy <glackycode@example.com>
2023-03-29 08:17:08 -0700

    fix issue

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,9 +9,12 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i;
-for (i = 0 ; i < n && *src != '\0' ; i++)
+for (i = 0 ; i < n && src[i] != '\0' ; i++)
 	dest[i] = src[i];
-for ( ; i < n ; i++)
-	*dest = '\0';
+while (i < n)
+{
+	dest[i] = '\0';
+	i++;
+}
 return (dest);
 }

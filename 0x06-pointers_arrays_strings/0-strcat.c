2425646b3c0cfdc09d02b1690734303afec1b6ac
glackyy <glackycode@example.com>
2023-03-29 03:34:27 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,12 +8,12 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	char *p = dest;
-	while (*dest != '\0')
-		dest++;
-	while (*src != '\0')
-		*dest++=*src++;
-	*dest = '\0';
-	return (p);
-
+char *p = dest;
+while (*dest != '\0')
+dest++;
+while (*src != '\0')
+*dest++ = *src++;
+*dest = '\0';
+return (p);
 }
+

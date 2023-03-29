726fe65a3b64bc1448c5fb5fd82c290184d49a82
glackyy <glackycode@example.com>
2023-03-29 03:56:04 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strncpy - Entry Function
+ * @dest: char
+ * @src: char
+ * @n: int
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
+for (i = 0 ; i < n && *src != 0 ; i++)
+	dest[i] = src[i];
+for ( ; i < n ; i++)
+	*dest = '\0';
+return (dest);
+}

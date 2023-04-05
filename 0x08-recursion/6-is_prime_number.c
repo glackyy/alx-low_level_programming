7260ae917e7b0e1ca5d697444f55522333a47ed7
glackyy <othmanesticky@gmail.com>
2023-04-05 05:26:13 -0700

    prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,13 +7,13 @@
  */
 int is_prime_number_help(int n, int i)
 {
-        if (n <= 1)
-                return (0);
-        if (i * i > n)
-                return (1);
-        if (n % i == 0)
-                return (0);
-        return (is_prime_number_help(n, i + 1));
+if (n <= 1)
+	return (0);
+if (i * i > n)
+	return (1);
+if (n % i == 0)
+	return (0);
+return (is_prime_number_help(n, i + 1));
 }
 /**
  * is_prime_number - Entry Function

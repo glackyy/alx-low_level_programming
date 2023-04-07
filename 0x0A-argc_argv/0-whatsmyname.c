a16a582434657d28bcd35421aa6bcea10677e3b5
glackyy <othmanesticky@gmail.com>
2023-04-07 04:55:41 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: int
+ * @argv: char
+ * Return: 0 (SUCCESS)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}

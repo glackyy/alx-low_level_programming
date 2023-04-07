f0393a9ae2fd54104ef6621591b21d8f559d22ac
glackyy <glackycode@example.com>
2023-04-07 05:04:06 -0700

    print number of arg passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: int
+ * @argv: char
+ * Return: 0 (SUCCESS)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}

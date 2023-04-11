29e12f29f752fb7f5e3fa2498af8f89acb585d6e
glackyy <othmanesticky@gmail.com>
2023-04-11 04:45:59 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+/**
+ * _strdup - Entry Function
+ * @str: char
+ * Return: str_strdup (SUCCESS)
+ */
+char *_strdup(char *str)
+{
+char *str_strdup = malloc((strlen(str) + 1) * sizeof(char));
+if (str == NULL)
+	return (NULL);
+if (str_strdup == NULL)
+	return (NULL);
+strcpy(str_strdup, str);
+return (str_strdup);
+}

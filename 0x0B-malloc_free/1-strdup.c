5a350a36898027d172c14f7f41335c53e24a9b45
glackyy <glackycode@example.com>
2023-04-11 05:11:37 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,11 +10,11 @@ char *_strdup(char *str)
 {
 int len = 0;
 int i;
-char *str_strdup = malloc((len + 1) * sizeof(char));
 while (str[len] != '\0')
 	len++;
 if (str == NULL)
 	return (NULL);
+char *str_strdup = malloc((len + 1) * sizeof(char));
 if (str_strdup == NULL)
 	return (NULL);
 for (i = 0 ; i <= len ; i++)

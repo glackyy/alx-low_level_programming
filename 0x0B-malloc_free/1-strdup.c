cde4127115535a0e54e2a17d62feecca96217008
glackyy <othmanesticky@gmail.com>
2023-04-11 05:09:45 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,7 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
-#include <string.h>
 /**
  * _strdup - Entry Function
  * @str: char
@@ -9,11 +8,16 @@
  */
 char *_strdup(char *str)
 {
-char *str_strdup = malloc((strlen(str) + 1) * sizeof(char));
+int len = 0;
+int i;
+char *str_strdup = malloc((len + 1) * sizeof(char));
+while (str[len] != '\0')
+	len++;
 if (str == NULL)
 	return (NULL);
-strcpy(str_strdup, str);
 if (str_strdup == NULL)
 	return (NULL);
+for (i = 0 ; i <= len ; i++)
+	str_strdup[i] = str[i];
 return (str_strdup);
 }

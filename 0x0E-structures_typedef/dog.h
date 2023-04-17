ce051fa70330821f186daca0f4fd00c875e8fcf4
glackyy <glackycode@example.com>
2023-04-17 04:52:40 -0700

    Struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
-struct dog {
+/**
+ * struct dog - Short description
+ * @name: char
+ * @age: float
+ * @owner: char
+ *
+ * Description: longer description
+ */
+struct dog
+{
 char *name;
 float age;
 char *owner;

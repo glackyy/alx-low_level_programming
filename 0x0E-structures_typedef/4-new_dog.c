096321caf4c90da3c072c029bfe7db90bb3272f6
glackyy <othmanesticky@gmail.com>
2023-04-17 05:55:55 -0700

    newdog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,19 +13,21 @@ dog_t *new_dog(char *name, float age, char *owner)
 dog_t *newdog = malloc(sizeof(dog_t));
 if (newdog == NULL)
 	return (NULL);
-newdog->name = strdup(name);
+newdog->name = malloc(strlen(name) + 1);
 if (newdog->name == NULL)
 {
 	free(newdog);
 	return (NULL);
 }
-newdog->age = age;
-newdog->owner = strdup(owner);
+strcpy(newdog->name, name);
+newdog->owner = malloc(strlen(owner) + 1);
 if (newdog->owner == NULL)
 {
 	free(newdog->name);
 	free(newdog);
 	return (NULL);
 }
+strcpy(newdog->owner, owner);
+newdog->age = age;
 return (newdog);
 }

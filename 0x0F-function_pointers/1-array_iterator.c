878760fe073835e8addd4a17231387c76bcc3a15
glackyy <glackycode@example.com>
2023-04-19 05:43:43 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,7 +7,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
+unsigned int i;
+if (array == NULL || action == NULL)
+	return;
 for (i = 0 ; i < size ; i++)
 	action(array[i]);
 }

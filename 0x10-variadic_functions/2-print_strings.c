a7a931ad44c1fea2ad04ea7627400cdc684d3d5a
glackyy <othmanesticky@gmail.com>
2023-04-20 04:56:25 -0700

    print strings

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,13 +14,13 @@ va_list args;
 va_start(args, n);
 for (i = 0 ; i < n ; i++)
 {
-	const char *str = va_arg(args, const char *);
-	if (str != NULL)
-		printf("%s", str);
-	else
-		printf("(nil)");
-	if (separator != NULL && i != n - 1)
-		printf("%s", separator);
+const char *str = va_arg(args, const char *);
+if (str != NULL)
+	printf("%s", str);
+else
+	printf("(nil)");
+if (separator != NULL && i != n - 1)
+	printf("%s", separator);
 }
 va_end(args);
 printf("\n");

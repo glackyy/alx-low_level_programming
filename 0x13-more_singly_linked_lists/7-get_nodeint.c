84e6a8a57a38e8f243af7c3978afd4c3d323b2e9
glackyy <glackycode@example.com>
2023-04-30 02:50:34 -0700

    get nodeint

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - Entry Function
+ * @head: Pointer
+ * @index: unsigned int
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *node = head;
+while (node != NULL && index > 0)
+{
+node = node->next;
+index--;
+}
+return (node);
+}

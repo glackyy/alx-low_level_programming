dd8505350cfc413a94dcbf51f15f7009a30359ad
glackyy <glackycode@example.com>
2023-05-08 06:43:34 -0700

    cp File_from File_to

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,16 @@
 #include "main.h"
+#define BUF_SIZE 1024
+/**
+ * print_error - Entry Function
+ * @err_message: Pointer
+ * @filename: Pointer
+ * @excode: int
+ */
+void print_error(const char *err_message, const char *filename, int excode)
+{
+fprintf(stderr, "Error: %s %s\n", err_message, filename);
+exit(excode);
+}
 /**
  * main - Entry Point
  * @argc: int
@@ -9,29 +21,26 @@ int main(int argc, char *argv[])
 {
 int filed_from, filed_to;
 ssize_t NbRead, NbWritten;
-char buf[BUFF_SIZE];
+char buf[BUF_SIZE];
 const char *file_from, *file_to;
 if (argc != 3)
 {
-	print_error("Usage: cp file_from file_to");
-	exit(97);
+	print_error("Usage: cp file_from file_to", "", 97);
 }
 file_from = argv[1];
 file_to = argv[2];
 filed_from = open(file_from, O_RDONLY);
 if (filed_from == -1)
 {
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-	exit(98);
+print_error("Error: Can't read from file %s\n", file_from, 98);
 }
 filed_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC,
 			S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 if (filed_to == -1)
 {
-	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
-	exit(99);
+print_error( "Error: Can't write to %s\n", file_to, 99);
 }
-while ((NbRead = read(filed_from, buf, BUFF_SIZE)) > 0)
+while ((NbRead = read(filed_from, buf, BUF_SIZE)) > 0)
 {
 	NbWritten = write(filed_to, buf, NbRead);
 	if (NbWritten == -1 || NbWritten != NbRead)
@@ -42,16 +51,11 @@ while ((NbRead = read(filed_from, buf, BUFF_SIZE)) > 0)
 }
 if (NbRead == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-exit(98);
+print_error("Error: Can't read from file %s\n", file_from, 98);
 }
-if (close(filed_from) == -1 || close(filed_to) == -1)
-{
-	if (close(filed_from) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close filed %d\n", filed_from);
-	if (close(filed_to) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close filed %d\n", filed_to);
-exit(100);
-}
+if (close(filed_from) == -1)
+print_error("Error: Can't close filed %d\n", "", 100);
+if (close(filed_to) == -1)
+print_error("Error: Can't close filed %d\n", "", 100);
 return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,11 +7,6 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <string.h>
-#define BUFF_SIZE 1024
-void print_error(const char *err_message)
-{
-dprintf(STDERR_FILENO, "%s\n", err_message);
-}
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);

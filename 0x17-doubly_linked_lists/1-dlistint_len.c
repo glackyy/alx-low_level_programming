3a0293c6788ccee432dd3654f2c9d42317a096dc
glackyy <othmanesticky@gmail.com>
2023-06-15 03:56:53 -0700

    dlistint len

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * dlistint_len - Entry function
+ * @h: Pointer
+ * Return: c
+ */
+size_t dlistint_len(const dlinstint_t *h)
+{
+size_t c = 0;
+while (h != NULL)
+{
+	c++;
+	h = h->next;
+}
+return (c);
+}

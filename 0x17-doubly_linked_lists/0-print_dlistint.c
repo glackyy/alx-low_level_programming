34ccf1a72a13af4cb1e413b4b1f2ff90182f3a4b
glackyy <othmanesticky@gmail.com>
2023-06-15 02:36:42 -0700

    print dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,22 +10,9 @@ size_t print_dlistint(const dlistint_t *h)
 size_t c = 0;
 while (h != NULL)
 {
-        print("%d\n", h->n);
-        h = h->next;
-        c++;
+	print("%d\n", h->n);
+	h = h->next;
+	c++;
 }
 return (c);
 }
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~                                                                                      
-~            

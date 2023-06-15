caace40f8e440ebaddc77a6d568e00828acdb5df
glackyy <glackycode@example.com>
2023-06-15 04:37:38 -0700

    sum dlistint

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * sum_dlistint - Entry function
+ * @head: Pointer
+ * Return: sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int sum = 0;
+while (head != NULL)
+{
+	sum += head->n;
+	head = head->next;
+}
+return (sum);
+}

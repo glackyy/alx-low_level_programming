8670172d6871e28f6cb4f92a8830315e79e1e986
glackyy <glackycode@example.com>
2023-07-07 05:25:18 -0700

    100

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -1,10 +1,4 @@
 #include "hash_tables.h"
-shash_table_t *shash_table_create(unsigned long int size);
-int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shash_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *ht);
-void shash_table_print_rev(const shash_table_t *ht);
-void shash_table_delete(shash_table_t *ht);
 /**
  * shash_table_create - Entry Function
  * @size: unsigned long int
@@ -153,7 +147,7 @@ if (ht == NULL)
 	return;
 printf("{");
 n = ht->stail;
-while (node != NULL)
+while (n != NULL)
 {
 	printf("'%s': '%s'", n->key, n->value);
 	if (n->sprev != NULL)
